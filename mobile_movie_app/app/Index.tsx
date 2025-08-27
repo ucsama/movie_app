9b04acabe836cd7dcfbef06b9356f290ba98088c
Nouman khan 27708 <27708@students.riphah.edu.pk>
2025-08-27 20:21:59 +0500

    styling
    initial styling

--- mobile_movie_app/app/Index.tsx
+++ mobile_movie_app/app/Index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Index() {
     return (
         <View className="flex-1 justify-center items-center bg-white">
-            <Text className="text-5xl text-purple-600 font-bold">Welcome!</Text>
+            <Text className="text-5xl text-dark-200 font-bold">Welcome back!</Text>
         </View>
     );
 }
