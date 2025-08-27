dc70effc75a386e4d92b75c6992d09757bd006f0
Nouman khan 27708 <27708@students.riphah.edu.pk>
2025-08-27 19:34:35 +0500

    Initial commit

---
+++ mobile_movie_app/app/Index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+    return (
+        <View className="flex-1 justify-center items-center bg-white">
+            <Text className="text-5xl text-purple-600 font-bold">Welcome!</Text>
+        </View>
+    );
+}
