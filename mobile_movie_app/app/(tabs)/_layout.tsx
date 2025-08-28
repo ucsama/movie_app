10e876ec9f07eda0cee55416ed6fa0108eecfe66
Nouman khan 27708 <27708@students.riphah.edu.pk>
2025-08-28 14:08:36 +0500

    hiding  headers of screens for now

--- mobile_movie_app/app/(tabs)/_layout.tsx
+++ mobile_movie_app/app/(tabs)/_layout.tsx
@@ -11,6 +11,27 @@ const _layout = () => {
                     headerShown: false
                 }}
             />
+
+            <Tabs.Screen
+            name="saved"
+            options={{
+            title: 'Saved',
+            headerShown: false}}
+            />
+
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false}}
+            />
+
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false}}
+            />
         </Tabs>
     )
 }
