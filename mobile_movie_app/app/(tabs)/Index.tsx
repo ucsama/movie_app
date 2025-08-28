293b584da19a99bc8c524d0bff88454f57e284ae
Nouman khan 27708 <27708@students.riphah.edu.pk>
2025-08-28 13:35:29 +0500

    setting up  required file structure
    setting up files for screens.

--- mobile_movie_app/app/(tabs)/Index.tsx
+++ mobile_movie_app/app/(tabs)/Index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import {Link} from "expo-router";
 
 export default function Index() {
     return (
         <View className="flex-1 justify-center items-center bg-white">
             <Text className="text-5xl text-dark-200 font-bold">Welcome back!</Text>
+
         </View>
     );
 }
